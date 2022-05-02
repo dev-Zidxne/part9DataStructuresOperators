9d1e56bc463626894d887a32ea22fed649887816
Zidane Innis <zidaneinnis@gmail.com>
2022-05-02 17:22:13 -0500

    Notes
    -Iterables are arrays, strings, maps, sets, NOT objects
    
    - Cannot use spread operator in tmeplate literal as value separated by commas are not expected there

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
   openingHours: {
     thu: {
       open: 12,
@@ -26,8 +25,109 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${
+        (this.starterMenu[starterIndex], this.mainMenu[mainIndex])
+      } and ${
+        this.mainMenu[mainIndex]
+      } will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
+const arr = [7, 8, 9];
+
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy Array
+
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//Join 2 Arrays
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+console.log(menu);
+
+// Iterables are arrays, strings, maps, sets, NOT objects
+
+const str = 'Jonas';
+
+const letters = [...str, '', 'S.'];
+
+console.log(letters);
+
+console.log(...str);
+console.log('j', 'o');
+
+// console.log(`${...str} Innis`); // cannot use spread operator in tmeplate literal as value separated by commas are not expected there
+
+/*
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+console.log(restaurantName, hours, tags);
+
+//Default Values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+
+console.log(menu, starters);
+
+//Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+//nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+
+///////////////////////////////////////
+// Destructuring Arrays
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -38,6 +138,7 @@ const [x, y, z] = arr;
 console.log(x, y, z);
 console.log(arr);
 
+// we can skip elements using a comma to skip as seen below
 let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
 
@@ -49,3 +150,22 @@ console.log(main, secondary);
 
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+
+console.log(starter, mainCourse);
+
+//Nested destructuring
+const nested = [2, 4, [5, 6]];
+
+// const [i, , j] = nested;
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//Default values
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+
+console.log(p, q, r); 
+*/
