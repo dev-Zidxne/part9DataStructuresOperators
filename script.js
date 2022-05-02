4b8dfb058e877318d48948cdbe518de7bdd0cf15
Zidane Innis <zidaneinnis@gmail.com>
2022-05-02 13:46:58 -0500

    Switching Varibles

--- script.js
+++ script.js
@@ -27,3 +27,25 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// Switching Variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
